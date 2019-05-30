b32f93c46e3feb89a23c30c0e9fb4fbd9052983c
Carlos <ceperez@Retina-MBP.fios-router.home>
2019-05-30 15:29:36 -0400

    add upload scripts with same path as current

--- tutorials/upload.sh
+++ tutorials/upload.sh
@@ -5,5 +5,5 @@ echo "uploading notebooks into repository"
 for file in `ls -1 $1/*.ipynb`
 do 
        	base=`basename $file .ipynb`	
-	knowledge_repo --repo ./project_repo add --update $file -p knowledge_repo_notebooks/$base
+	knowledge_repo --repo ./project_repo add --update $file -p tutorials/$base
 done
